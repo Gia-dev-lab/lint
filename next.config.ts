9b4c0dd631cf044a54197fa660d23273c8733dfb
Giacomo Ansani <giacomo.ansani@gmail.com>
2025-10-23 16:50:15 +0000

    non si carica

--- next.config.ts
+++ next.config.ts
@@ -23,17 +23,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'www.lintmicrofibercloths.it',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
