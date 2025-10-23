763cdad61e6cd702fed4ba3275a99c91a75d7e42
Giacomo Ansani <giacomo.ansani@gmail.com>
2025-10-23 17:09:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8238432636-f26e0",
+  "appId": "1:936219711075:web:800e8a37a595de2efd4a56",
+  "apiKey": "AIzaSyDnnMd3PAbcPmRevN2zmh4OkejEo6Br5F4",
+  "authDomain": "studio-8238432636-f26e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "936219711075"
+};
