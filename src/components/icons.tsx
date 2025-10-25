2102504857577b2c511891c9b8fe89e6a8a4baeb
Giacomo Ansani <giacomo.ansani@gmail.com>
2025-10-25 20:00:17 +0000

    usa questo logo: https://www.lintmicrofibercloths.it/wp-content/uploads/

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div className="flex items-center gap-2" {...props}>
-      <Image src="/images/logo.svg" alt="Lint Logo" width={80} height={32} />
-      <span className="text-xl font-bold text-foreground">Lint <span className="font-normal">Professional Cleaning</span></span>
+      <Image src="https://www.lintmicrofibercloths.it/wp-content/uploads/2024/10/lintlogo.png" alt="Lint Logo" width={100} height={32} style={{ filter: 'brightness(0) invert(1)'}} className="dark:invert-0" />
     </div>
   );
 }
