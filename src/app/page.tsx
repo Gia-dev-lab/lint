bdaa258cacfb576ddd5ea4d3009d96d4b384c180
Giacomo Ansani <giacomo.ansani@gmail.com>
2025-10-26 18:15:42 +0000

    https://www.lintmicrofibercloths.it/wp-content/uploads/2024/10/CARTA-BIA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ const solutionCategories = [
   {
     title: "Panni Autolavaggio",
     link: "/prodotti",
-    image: "https://images.unsplash.com/photo-1554158488-a7f402c0380f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://www.lintmicrofibercloths.it/wp-content/uploads/2024/10/CARTA-BIANCA-SELF.webp",
     imageHint: "autolavaggio",
   },
   {
